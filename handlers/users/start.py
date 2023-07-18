69da979a37cee470a7eab00473bf918b7a9b0fe8
avlazarev <alekse.lazarev.01@gmail.com>
2023-07-19 00:11:17 +0300

    добавил stop

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -4,7 +4,7 @@ import asyncpg
 import gspread_asyncio
 from aiogram import types
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.builtin import CommandStart
+from aiogram.dispatcher.filters import CommandStart, Command
 from aiogram.types import ReplyKeyboardRemove
 from google.oauth2.service_account import Credentials
 
@@ -43,6 +43,59 @@ async def add_worksheet(async_spreadsheet: gspread_asyncio.AsyncioGspreadSpreads
 async def bot_start(message: types.Message, state: FSMContext):
     await message.answer("Введите ФИО клиента", reply_markup=ReplyKeyboardRemove())
     await state.set_state("Имя клиента")
+
+@dp.message_handler(Command("stop"), state='*')
+@dp.message_handler(state="Дополнительные комментарии")
+async def state_data_gsheets(message: types.Message, state: FSMContext):
+    if message.text == "/stop":
+        comment = ""
+    else:
+        comment = message.text
+    await state.update_data(comment=comment)
+
+    today = datetime.date.today()
+    data = await state.get_data()
+    await message.answer(f"1. {message.from_user.full_name}\n"
+                         f"2. {today.strftime('%d.%m.%y')}\n"
+                         f"3. {data.get('client_name')}\n"
+                         f"4. {data.get('have_area')}\n"
+                         f"5. {data.get('location')}\n"
+                         f"6. {data.get('construction_technology')}\n"
+                         f"7. {data.get('prod_line')}\n"
+                         f"8. {data.get('project')}\n"
+                         f"9. {data.get('date_of_starting')}\n"
+                         f"10. {data.get('source_of_financing')}\n"
+                         f"11. {data.get('construction_budget')}\n"
+                         f"12. {data.get('phone_number')}\n"
+                         f"13. {data.get('email')}\n"
+                         f"14. {data.get('way_of_communication')}\n"
+                         f"15. {data.get('comment')}\n",
+                         reply_markup=start_keyboard)
+    await state.reset_state()
+    spreadsheet_id = '1IYUDIj8w9RLePTBgof00mmWvXZnbtvGeuRNuyMznMN0'
+    client = gspread_asyncio.AsyncioGspreadClientManager(get_scoped_credentials(PATH))  # импорт из конфига
+    client = await client.authorize()
+    async_spreadsheet = await client.open_by_key(spreadsheet_id)
+
+    worksheet = await async_spreadsheet.worksheet('Лист1')
+    values = [
+        message.from_user.full_name,
+        today.strftime('%d.%m.%y'),
+        data.get('client_name'),
+        data.get('have_area'),
+        data.get('location'),
+        data.get('construction_technology'),
+        data.get('prod_line'),
+        data.get('project'),
+        data.get('date_of_starting'),
+        data.get('source_of_financing'),
+        data.get('construction_budget'),
+        data.get('phone_number'),
+        data.get('email'),
+        data.get('way_of_communication'),
+        data.get('comment'),
+              ]
+    await worksheet.append_row(values)
 
 
 @dp.message_handler(state="Имя клиента")
@@ -134,44 +187,6 @@ async def state_data_gsheets(message: types.Message, state: FSMContext):
 async def state_data_gsheets(message: types.Message, state: FSMContext):
     way_of_communication = message.text
     await state.update_data(way_of_communication=way_of_communication)
-    today = datetime.date.today()
-    data = await state.get_data()
-    await message.answer(f"1. {message.from_user.full_name}\n"
-                         f"2. {today.strftime('%d.%m.%y')}\n"
-                         f"3. {data.get('client_name')}\n"
-                         f"4. {data.get('have_area')}\n"
-                         f"5. {data.get('location')}\n"
-                         f"6. {data.get('construction_technology')}\n"
-                         f"7. {data.get('prod_line')}\n"
-                         f"8. {data.get('project')}\n"
-                         f"9. {data.get('date_of_starting')}\n"
-                         f"10. {data.get('source_of_financing')}\n"
-                         f"11. {data.get('construction_budget')}\n"
-                         f"12. {data.get('phone_number')}\n"
-                         f"13. {data.get('email')}\n"
-                         f"14. {data.get('way_of_communication')}\n",
-                         reply_markup=start_keyboard)
-    await state.reset_state()
-    spreadsheet_id = '1IYUDIj8w9RLePTBgof00mmWvXZnbtvGeuRNuyMznMN0'
-    client = gspread_asyncio.AsyncioGspreadClientManager(get_scoped_credentials(PATH))  # импорт из конфига
-    client = await client.authorize()
-    async_spreadsheet = await client.open_by_key(spreadsheet_id)
+    await message.answer("Дополнительные комментарии", reply_markup=messengers_list_keyboard)
+    await state.set_state("Дополнительные комментарии")
 
-    worksheet = await async_spreadsheet.worksheet('Лист1')
-    values = [
-        message.from_user.full_name,
-        today.strftime('%d.%m.%y'),
-        data.get('client_name'),
-        data.get('have_area'),
-        data.get('location'),
-        data.get('construction_technology'),
-        data.get('prod_line'),
-        data.get('project'),
-        data.get('date_of_starting'),
-        data.get('source_of_financing'),
-        data.get('construction_budget'),
-        data.get('phone_number'),
-        data.get('email'),
-        data.get('way_of_communication')
-              ]
-    await worksheet.append_row(values)
