143de821cb6e9b4c9705e193af019d2d253c3afd
avlazarev <alekse.lazarev.01@gmail.com>
2023-07-19 00:02:33 +0300

    убрал лишнее

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,8 +4,5 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
         types.BotCommand("start", "Запустить бота"),
-        types.BotCommand("stop", "Закончить анкету (с сохранением)"),
-        types.BotCommand("help", "Помощь"),
-        types.BotCommand("cancel", "Отмена"),
 
     ])
